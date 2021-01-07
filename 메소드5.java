643a9c9f7af33f1628751cff3de598d09da90006
2theMoon <dlaguddh1@gmail.com>
2021-01-07 20:42:39 +0900

    Add files via upload

---
+++ 메소드5.java
@@ -0,0 +1,19 @@
+package 메소드;
+
+public class 메소드5 {
+
+	public static void main(String[] args) {
+		// 안녕하세요 라고 표시하는 메소드 작성
+		// 실행결과
+		// 안녕하세요.
+		// 안녕하세요.
+		// 안녕하세요.
+
+		hello();
+		hello();
+		hello();
+	}
+static void hello() {
+	System.out.println("안녕하세요.");
+}
+}
